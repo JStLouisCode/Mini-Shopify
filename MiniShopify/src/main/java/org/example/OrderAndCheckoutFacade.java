04e69c800cecd895cae2f00b505171a16df1f51f
BrockFielder <163914636+BrockFielder@users.noreply.github.com>
2025-10-21 17:31:15 -0400

    Added basic functions and such in
    Added some basic functioanlity that I think will allow one to use the ShopManagmentFacade

--- MiniShopify/src/main/java/org/example/OrderAndCheckoutFacade.java
+++ MiniShopify/src/main/java/org/example/OrderAndCheckoutFacade.java
@@ -1,4 +1,28 @@
 package org.example;
 
 public class OrderAndCheckoutFacade {
+    private ShopManagementFacade shop;
+    private static OrderAndCheckoutFacade instance;
+
+    public OrderAndCheckoutFacade(ShopManagementFacade shop) {
+        this.shop = shop;
+    }
+
+    public void orderSomething (int productID){
+        shop.getProducts();
+            for (ProductManagementFacade p : shop.getProducts()){
+
+                if (p.getQuantity() > 0){
+                    if (p.getProductID() == productID) {
+                        p.setQuantityMinus1();
+                    }
+                    else {
+                        System.out.println("There is no more of this product, error in ordering");
+                    }
+                }
+
+            }
+    }
+
+
 }
--- MiniShopify/src/main/java/org/example/ShopManagementFacade.java
+++ MiniShopify/src/main/java/org/example/ShopManagementFacade.java
@@ -1,4 +1,61 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class ShopManagementFacade {
+
+    private String name;
+    private ArrayList<String> tags;
+    private ArrayList<ProductManagementFacade> products;
+    private String ShopType;
+    private UserMangementFacade owner;
+    private int id;
+
+    public ShopManagementFacade(UserMangementFacade owner, int id) {
+        this.owner = owner;
+        this.id = id;
+
+
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(ArrayList<String> tags) {
+        this.tags = tags;
+    }
+
+    public String getShopType() {
+        return ShopType;
+    }
+
+    public void setShopType(String shopType) {
+        ShopType = shopType;
+    }
+
+    //No setters for owner and ID for now for safety reasons
+    public UserMangementFacade getOwner() {
+        return owner;
+    }
+
+    public int getId() {return id;}
+
+    public void createProduct (String productName, int productID, String productDescription, double productPrice, String productCategory, int quantity) {
+        products.add(new ProductManagementFacade(productName, productID,productDescription,productPrice,productCategory, quantity, this));
+    }
+
+    public ArrayList<ProductManagementFacade> getProducts() {
+        return products;
+    }
 }
